40e176ff717c39e25b1d19a3a884a3298b166168
Simran Pathan <97839297+Simran-sam@users.noreply.github.com>
2025-03-17 14:57:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let obj= {};
+	for(let char of str){
+		if (obj[char]===undefined) {
+			obj[char]=0; 
+		}
+		obj[char]+=1;
+	}
+	for(let char of str){
+		if (obj[char]===1) {
+			return char;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
